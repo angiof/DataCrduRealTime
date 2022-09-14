0348cf0519ed6e680063fa8fb914bb3f2b754141
angelo ferretti <31105678+angiof@users.noreply.github.com>
2022-09-14 18:41:27 +0200

    add delate btn and her fun

--- app/src/main/java/com/example/datacrdurealtime/views/MainActivity.kt
+++ app/src/main/java/com/example/datacrdurealtime/views/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.datacrdurealtime.views
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -17,8 +20,10 @@ import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var empList: ArrayList<Persona>  //lista del modello
-    val db = FirebaseDatabase.getInstance().reference
-    val myReference = db.child("persona") // reference patch
+
+        val db = FirebaseDatabase.getInstance().reference
+        val myReference = db.child("persona") // reference patch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,31 +32,24 @@ class MainActivity : AppCompatActivity() {
         setAdderIUSers()
 
 
-
-
     }
 
     fun getLista() {
         empList = arrayListOf()
-
         val listainer = object : ValueEventListener {
             override fun onDataChange(snap: DataSnapshot) {
                 empList.clear()
-
                 for (data in snap.children) {
                     val datiSnap =
                         data.getValue(Persona::class.java) //modella i risultati dell'iterazione dentro la classe modello
                     empList.add(datiSnap!!)   //aggiugne i dati della classe modello all'interno della lista
                 }
-
                 val myAdapter = MyAdapter()
-
                 findViewById<RecyclerView>(R.id.recy_mian).apply {
                     this.setHasFixedSize(true)
                     myAdapter.submitList(empList)
                     this.adapter = myAdapter
                 }
-
             }
 
             override fun onCancelled(p0: DatabaseError) {
@@ -62,7 +60,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun setAdderIUSers(){
+    fun setAdderIUSers() {
         findViewById<Button>(R.id.btn_main_adder).setOnClickListener {
             val nomeID = findViewById<EditText>(R.id.tx_nome).text.toString()
             val desc = findViewById<EditText>(R.id.tx_desc).text.toString()
@@ -73,34 +71,19 @@ class MainActivity : AppCompatActivity() {
             val listainerAdder = db.addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
 
-
                     myReference.child(nome).setValue(oggpersona).addOnSuccessListener {
                     }.addOnFailureListener {
 
                         Toast.makeText(this@MainActivity, "no", Toast.LENGTH_SHORT).show()
                     }
-
-
                 }
 
                 override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(this@MainActivity, error.message, Toast.LENGTH_SHORT).show()
                 }
-
             })
-            findViewById<Button>(R.id.bt_cancella).setOnClickListener {
-
-                myReference.child(nome).
-                removeValue().addOnCompleteListener {
-                    Toast.makeText(this@MainActivity, "rimosso", Toast.LENGTH_SHORT).show()
-                }
-
-
-            }
 
             myReference.addValueEventListener(listainerAdder)
-
-
         }
     }
-
 }
--- app/src/main/java/com/example/datacrdurealtime/views/MyAdapter.kt
+++ app/src/main/java/com/example/datacrdurealtime/views/MyAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.datacrdurealtime.views
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
@@ -8,6 +9,15 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.datacrdurealtime.databinding.ListaBinding
 import com.example.datacrdurealtime.dto.Persona
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import java.util.*
 
 class MyAdapter() :
     ListAdapter<Persona, MyAdapter.HolderCrud>(DiffCallBack()) {
@@ -17,9 +27,16 @@ class MyAdapter() :
         fun binder(persona: Persona?) {
             bindingLista.apply {
                 this.tvId.text = persona?.id
-                this.tvDesc.text= persona?.desc
+                this.tvDesc.text = persona?.desc
                 this.card.setOnClickListener {
                     Toast.makeText(this.card.context, persona?.id, Toast.LENGTH_SHORT).show()
+                }
+                this.btCancella.setOnClickListener {
+                    if (!persona?.id.isNullOrEmpty()) {
+                        cancelUser(persona?.id.toString(), this.card.context)
+                    } else {
+                        Toast.makeText(it.context, "errroe ", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
@@ -32,11 +49,32 @@ class MyAdapter() :
 
     override fun onBindViewHolder(holder: HolderCrud, position: Int) {
         val binding = getItem(position)
-            holder.binder(binding)
+        holder.binder(binding)
 
     }
 
+    fun cancelUser(unser: String, context: Context)  {
+        val db = FirebaseDatabase.getInstance().reference
+        val myReference = db.child("persona")
+
+        val listainer = object :ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+            }
+        }
+
+            //val listainer = myReference.child(unser).removeValue().addOnCompleteListener {
+        db.addListenerForSingleValueEvent(listainer)
+
+    }
+
+
 }
+
 
 class DiffCallBack() : DiffUtil.ItemCallback<Persona>() {
     override fun areItemsTheSame(oldItem: Persona, newItem: Persona) = oldItem.id == newItem.id
