677f0b191e2e5ee1b7805bc510606fa91ae6db5e
angelo ferretti <angiokop@gmail.com>
2022-09-14 18:50:29 +0200

    add second actvity for test with querry fun

---
+++ app/src/main/java/com/example/datacrdurealtime/views/MainActivity2.kt
@@ -0,0 +1,37 @@
+package com.example.datacrdurealtime.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.datacrdurealtime.R
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        
+        delate()
+
+
+    }
+
+    fun delate() {
+        findViewById<Button>(R.id.btn_main2_querry).setOnClickListener {
+            val db = FirebaseDatabase.getInstance().reference
+            val myReference = db.child("persona")
+            val id: String? = findViewById<EditText>(R.id.tx_desc_2).text.toString()
+            val desc: String? = findViewById<EditText>(R.id.tx_id_2).text.toString()
+
+
+            myReference.child(id.toString()).removeValue().addOnCompleteListener {
+                Toast.makeText(this, "rimosso", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "erroee", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+}
