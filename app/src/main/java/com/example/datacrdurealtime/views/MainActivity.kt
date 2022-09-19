132a67efa5909eb614a22431d52e117f3d39add3
angelo ferretti <angiokop@gmail.com>
2022-09-20 00:12:48 +0200

    add Email User Register
    use frirebase sdk login authentication

--- app/src/main/java/com/example/datacrdurealtime/views/MainActivity.kt
+++ app/src/main/java/com/example/datacrdurealtime/views/MainActivity.kt
@@ -5,18 +5,32 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.RecyclerView
 import com.example.datacrdurealtime.R
 import com.example.datacrdurealtime.dto.Persona
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.auth.api.Auth
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthMultiFactorException
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import java.security.AuthProvider
 import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var empList: ArrayList<Persona>  //lista del modello
-    companion object{
+    private lateinit var firebaseAuthStateListainer: FirebaseAuth.AuthStateListener
+
+    companion
+
+    object {
         val db = FirebaseDatabase.getInstance().reference
         val myReference = db.child("persona") // reference patch
     }
@@ -24,7 +38,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getLista()
+        integrateAU()
+
     }
 
     fun getLista() {
@@ -45,6 +60,7 @@ class MainActivity : AppCompatActivity() {
                     myAdapter.submitList(empList)
                 }
             }
+
             override fun onCancelled(p0: DatabaseError) {
             }
         }
@@ -57,9 +73,9 @@ class MainActivity : AppCompatActivity() {
             val desc = findViewById<EditText>(R.id.tx_desc).text.toString()
             val nome: String = nomeID
             val descT: String = desc
-            if (nome.isEmpty()or descT.isEmpty() or snap.hasChild(nome)){
+            if (nome.isEmpty() or descT.isEmpty() or snap.hasChild(nome)) {
                 Toast.makeText(this, "non rompere il progetto ", Toast.LENGTH_SHORT).show()
-            }else{
+            } else {
                 val oggpersona = Persona(nome, descT)
                 myReference.child(nome).setValue(oggpersona).addOnSuccessListener {
                 }.addOnFailureListener {
@@ -68,5 +84,26 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+    fun integrateAU() {
+        //states
+        firebaseAuthStateListainer = FirebaseAuth.AuthStateListener { auth ->
+            if (auth.currentUser != null) {
+                supportActionBar?.title= auth.currentUser?.displayName.toString()
+            }
+        }
+
+
+        val provider = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            val response = IdpResponse.fromResultIntent(it.data)
+            if (it.resultCode == RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+                if (user == null) {
+                    Toast.makeText(this, "ciaooo", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }.launch(AuthUI.getInstance().createSignInIntentBuilder().build())
     }
 }
